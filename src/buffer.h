3436a61100013374eca74f60b1d924983c2e7e67
Michael Iatrou <michael.iatrou@freemail.gr>
2008-07-18 11:28:56 +0300

    Add bless_buffer_copy() function.

--- src/buffer.h
+++ src/buffer.h
@@ -17,10 +17,9 @@ typedef int (*bless_progress_cb)(void *);
 
 bless_buffer_t *bless_buffer_new();
 
-bless_buffer_t *bless_buffer_open(const char *path, int mode);
+bless_buffer_t *bless_buffer_open(int fd, int mode);
 
-int bless_buffer_save(bless_buffer_t *buf, const char *path,
-		bless_progress_cb cb);
+int bless_buffer_save(bless_buffer_t *buf, int fd, bless_progress_cb cb);
 
 int bless_buffer_close(bless_buffer_t *buf);
 
@@ -32,6 +31,9 @@ int bless_buffer_insert(bless_buffer_t *buf, off_t offset,
 		void *data, size_t len);
 
 int bless_buffer_delete(bless_buffer_t *buf, off_t offset, size_t len);
+
+int bless_buffer_copy(bless_buffer_t *src, off_t skip, bless_buffer_t *dst,
+		off_t seek, size_t len);
 
 off_t bless_buffer_find(bless_buffer_t *buf, off_t start_offset, 
 		void *data, size_t len, bless_progress_cb cb);
