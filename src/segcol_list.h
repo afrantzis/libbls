d63e548b15311f8817603d7bea1149e0225ed821
alf82@freemail.gr <>
2008-10-29 01:51:11 +0200

    + Added forgotten file from previous commit.

---
+++ src/segcol_list.h
@@ -0,0 +1,14 @@
+/**
+ * @file segcol_list.h
+ *
+ * Definition of constructor function for the list implementation of segcol_t.
+ */
+#ifndef _SEGCOL_LIST_H
+#define _SEGCOL_LIST_H
+
+#include "segcol.h"
+
+int segcol_list_new(segcol_t **segcol);
+
+#endif
+
