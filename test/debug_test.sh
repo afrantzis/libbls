6423a908c6a5df6b734f8f3ff0c3acfa60e5b289
Alexandros Frantzis <alf82@freemail.gr>
2011-10-01 13:00:23 +0300

    Update test debugging scripts with new build paths and sanity checks.

--- test/debug_test.sh
+++ test/debug_test.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 # Run test in gdb. Make sure to have compiled the code (and optionally the bindings)
-# with debug symbols (scons debug=1)
+# with debug symbols
 
-LD_LIBRARY_PATH=../build/default/src PYTHONPATH=../build/default/bindings gdb --args python $1
+BUILD_DIR=../build/visibility-public
+
+if [ ! -f $BUILD_DIR/bindings/_libbls.so ]; then
+	echo "You need to run './waf test' first to build the test infrastructure."
+	exit
+fi
+
+LD_LIBRARY_PATH=$BUILD_DIR/src PYTHONPATH=$BUILD_DIR/bindings gdb --args python $1
--- test/valgrind_test.sh
+++ test/valgrind_test.sh
@@ -2,4 +2,11 @@
 
 # Run test in valgrind.
 
-LD_LIBRARY_PATH=../build/default/src PYTHONPATH=../build/default/bindings valgrind --leak-check=full python $1
+BUILD_DIR=../build/visibility-public
+
+if [ ! -f $BUILD_DIR/bindings/_libbls.so ]; then
+	echo "You need to run './waf test' first to build the test infrastructure."
+	exit
+fi
+
+LD_LIBRARY_PATH=$BUILD_DIR/src PYTHONPATH=$BUILD_DIR/bindings valgrind --leak-check=full python $1
